3b0955717a049aa4a94120212da273c8585d3389
Shanu M <shanum@Shanus-MacBook-Pro.local>
2020-01-03 15:08:16 +0530

    Shanu: Cleanup/Reformating of the code

--- src/main/kotlin/com/idli/App.kt
+++ src/main/kotlin/com/idli/App.kt
@@ -3,17 +3,18 @@ package com.idli
 import com.idli.datatypes.oracle.OracleTypes
 import com.idli.extensions.parseLineToColumnValues
 import com.idli.extensions.parseLineToColumns
+import com.idli.model.ColumnBuilder
 import com.idli.model.Table
 import java.io.File
 
 fun main() {
     val randomLines = File("<<PathToFile>>")
-        .bufferedReader().useLines { lines ->
-            lines.filterIndexed { index, _ -> index == 0 || index % 23 == 0 }.toList()
+        .bufferedReader().useLines {
+            it.filterIndexed { index, _ -> index == 0 || index % 23 == 0 }.toList()
         }
     val columnsBuilders = randomLines.first().parseLineToColumns(OracleTypes())
     val columns =
         randomLines.drop(1).fold(columnsBuilders, operation = { acc, s -> s.parseLineToColumnValues(acc) })
-            .map { it.build() }
+            .map(ColumnBuilder::build)
     print(Table("sample", columns).createStatement())
 }
